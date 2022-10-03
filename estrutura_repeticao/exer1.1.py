b6046ea915f1359703c0e30947a101a33417ad71
luiztoledosilva <89946718+luiztoledosilva@users.noreply.github.com>
2022-10-02 23:04:29 -0300

    estgruturas de repeticao

---
+++ estrutura_repeticao/exer1.1.py
@@ -0,0 +1,5 @@
+def multiplos(a):
+   for a in range(0,16,3):
+        print(a, end=' ')
+n=0
+print(multiplos(n))
---
+++ estrutura_repeticao/exer1.py
@@ -0,0 +1,8 @@
+
+###sem funcao abaixo 
+
+for i in range(0,16,3):
+    ##print(i)
+    print(i, end=' ')
+    
+    ##com funcao abaixo
